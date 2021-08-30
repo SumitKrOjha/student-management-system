5d1ccb6df1fb76bfd269d18a84cdc28dd05389c3
Sumit Kumar Ojha <sumitojha028@gmail.com>
2021-08-30 22:38:42 +0530

    initial add from Spring Initializer. Closes #1

---
+++ README.md
@@ -0,0 +1,2 @@
+# StudentManagementSystem
+A simple Student Management System web application using Spring Boot, Spring MVC, Thymeleaf, Spring Data JPA, and PostgreSQL database.
---
+++ src/test/java/com/sumit/studentmanagementsystem/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumit.studentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
